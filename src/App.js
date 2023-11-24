37cb26af04b4b4e067f1c14a180156b5d326f7d3
RAZEX0LOL <khattayev00@mail.ru>
2023-11-24 19:52:55 +0400

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	deleted:    public/favicon.ico
    	new file:   public/img/cart.svg
    	new file:   public/img/logo.png
    	new file:   public/img/user.svg
    	modified:   public/index.html
    	deleted:    public/logo192.png
    	deleted:    public/logo512.png
    	deleted:    public/manifest.json
    	deleted:    public/robots.txt
    	deleted:    src/App.css
    	modified:   src/App.js
    	deleted:    src/App.test.js
    	deleted:    src/index.css
    	modified:   src/index.js
    	new file:   src/index.scss
    	deleted:    src/logo.svg
    	deleted:    src/reportWebVitals.js
    	deleted:    src/setupTests.js
    
     Changes not staged for commit:
    	modified:   public/index.html
    	modified:   src/App.js
    	modified:   src/index.scss
    
     Untracked files:
    ъ	public/img/Plus.svg
    	public/img/Search.svg
    	public/img/btn-checked.svg
    	public/img/btn-plus.svg
    	public/img/heart-like.svg
    	public/img/heart-unlike.svg
    	public/img/phones/NothingPhone2.jpg
    	public/img/phones/SamsungS23Ultra.jpg
    	public/img/phones/iPhone15Plus.png
    	public/img/phones/iPhone15Pro.jpg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper clear">
+     <header className="d-flex justify-between align-center p-40">
+      <div className="d-flex align-center">
+        <img width={40} height={40} src="./img/logo.png"/>
+        <div>
+          <h3 className="text-uppercase">React Phone</h3>
+          <p>Магазин качественных смартфонов</p>
+        </div>
+      </div>
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img width={18} height={18} src="./img/cart.svg"/>
+            <span>1000 руб.</span>
+          </li>
+          <li>
+            <img width={18} height={18} src="./img/user.svg"/>
+          </li>
+        </ul>
+     </header>
+      <div className="content p-40">
+        <h1>Все смартфоны</h1>
+        ...............
+      </div>
     </div>
   );
 }
 
-export default App;
