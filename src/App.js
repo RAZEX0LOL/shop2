74edd0ea35d6310b5e38b9fdde7a2ed40cb1fa64
RAZEX0LOL <khattayev00@mail.ru>
2023-11-24 19:55:43 +0400

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   public/img/Plus.svg 	new file:   public/img/Search.svg 	new file:   public/img/btn-checked.svg 	new file:   public/img/btn-plus.svg 	new file:   public/img/heart-like.svg 	new file:   public/img/heart-unlike.svg 	new file:   public/img/phones/NothingPhone2.jpg 	new file:   public/img/phones/SamsungS23Ultra.jpg 	new file:   public/img/phones/iPhone15Plus.png 	new file:   public/img/phones/iPhone15Pro.jpg 	modified:   public/index.html 	modified:   src/App.js 	modified:   src/index.scss

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
         <img width={40} height={40} src="./img/logo.png"/>
         <div>
           <h3 className="text-uppercase">React Phone</h3>
-          <p>Магазин качественных смартфонов</p>
+          <p className="opacity-5">Магазин качественных смартфонов</p>
         </div>
       </div>
         <ul className="d-flex">
@@ -20,8 +20,70 @@ export default function App() {
         </ul>
      </header>
       <div className="content p-40">
-        <h1>Все смартфоны</h1>
-        ...............
+        <div className="d-flex aling-center justify-between mb-40">
+          <h1>Все смартфоны</h1>
+          <div className="search-block d-flex">
+            <img src="./img/Search.svg" alt="Search"/>
+            <input placeholder="Поиск..."/>
+          </div>
+        </div>
+        <div className="d-flex">
+        <div className="card">
+          <div className="favorite">
+            <img src="./img/heart-unlike.svg" alt="Unliked"/>
+          </div>
+          <img width={133} height={112} src="./img/phones/iPhone15Plus.png" alt="Phone"/>
+          <h5>Apple iPhone 15 Plus 128Gb</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена:</span>
+              <b>99 999 руб.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="./img/Plus.svg" alt="Plus"/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={112} src="./img/phones/iPhone15Pro.jpg" alt="Phone"/>
+          <h5>Apple iPhone 15 Plus 128Gb</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена:</span>
+              <b>99 999 руб.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="./img/Plus.svg" alt="Plus"/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={112} src="./img/phones/NothingPhone2.jpg" alt="Phone"/>
+          <h5>Apple iPhone 15 Plus 128Gb</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена:</span>
+              <b>99 999 руб.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="./img/Plus.svg" alt="Plus"/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={112} src="./img/phones/SamsungS23Ultra.jpg" alt="Phone"/>
+          <h5>Apple iPhone 15 Plus 128Gb</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена:</span>
+              <b>99 999 руб.</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="./img/Plus.svg" alt="Plus"/>
+            </button>
+          </div>
+        </div>
+        </div>
       </div>
     </div>
   );
