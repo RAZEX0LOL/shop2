c2032e4b78f65b5f2924179004303cb0bd2e0033
RAZEX0LOL <khattayev00@mail.ru>
2023-12-04 17:08:18 +0400

    add images, reorganize card.jsx and his styles in new directory

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import Card from "./components/Card.jsx";
+import React from "react";
+import Card from "./components/Card";
 import Drawer from "./components/Drawer.jsx";
 import Header from "./components/Header.jsx";
 
+const arr=[
+  {
+    title:'Apple iPhone 15 Plus 128GB',
+    price: 99999,
+    imageUrl: './img/phones/iPhone15Plus.png'
+  },
+  {
+    title:'Apple iPhone 15 Pro 512GB',
+    price: 159999,
+    imageUrl: './img/phones/iPhone15Pro.png'
+  },
+  {
+    title:'Nothing Phone 2 256GB',
+    price: 59999,
+    imageUrl: './img/phones/NothingPhone2.png'
+  },
+  {
+    title:'Samsung S23 Ultra 512GB',
+    price: 119999,
+    imageUrl: './img/phones/SamsungS23Ultra.png'
+  },
+  {
+    title:'',
+    price: 119999,
+    imageUrl: './img/phones/SamsungS23Ultra.png'
+  },
+];
+
 export default function App() {
+  const [cartOpened, setCartOpened]=React.useState(false);
+
+
+
   return (
     <div className="wrapper clear">
-      <Drawer/>
-      <Header/>
+      {cartOpened && <Drawer onClose={()=>setCartOpened(false)}/>}
+      <Header onClickCart={()=>setCartOpened(true)}/>
       <div className="content p-40">
         <div className="d-flex aling-center justify-between mb-40">
           <h1>Все смартфоны</h1>
@@ -16,10 +49,10 @@ export default function App() {
           </div>
         </div>
         <div className="d-flex">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+          {arr.map((obj)=>(
+            <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} />
+          ))}
+          {/* <Card title="Apple iPhone 15 Pro 512Gb" price={159999} imageUrl="./img/phones/iPhone15Pro.jpg"/> */}
         </div>
       </div>
     </div>
